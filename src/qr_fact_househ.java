8ff626a1294837f81f4fae781482a051b7e8242b
anjali <anjali@anjalipc>
2014-11-23 12:16:54 -0500

    change in Q R- no null

--- src/qr_fact_househ.java
+++ src/qr_fact_househ.java
@@ -62,82 +62,6 @@ public class qr_fact_househ {
         //System.out.println(qr.toString());
       
     }
-    
-    public void QRDecomposition (RealMatrix a) {
-        // Initialize.
-    	RealMatrix A  = a.copy();
-        QR = A.getData();
-       int  m = A.getRowDimension();
-        int n = A.getColumnDimension();
-         Rdiag = new double[n];
-
-        // Main loop.
-        for (int k = 0; k < n; k++) {
-           // Compute 2-norm of k-th column without under/overflow.
-           double nrm = 0;
-           for (int i = k; i < m; i++) {
-              nrm = Math.hypot(nrm,QR[i][k]);
-           }
-           if (nrm != 0.0) {
-              // Form k-th Householder vector.
-              if (QR[k][k] < 0) {
-                 nrm = -nrm;
-              }
-              for (int i = k; i < m; i++) {
-                 QR[i][k] /= nrm;
-                 //System.out.println("qr ik" + QR[i][k]);
-              }
-              QR[k][k] += 1.0;
-            
-              // Apply transformation to remaining columns.
-              for (int j = k+1; j < n; j++) {
-                 double s = 0.0; 
-                 for (int i = k; i < m; i++) {
-                    s += QR[i][k]*QR[i][j];
-                 }
-                 s = -s/QR[k][k];
-                 //System.out.println("hh s" + j + " " + s);
-                 for (int i = k; i < m; i++) {
-                    QR[i][j] += s*QR[i][k];
-                  //System.out.println("ij " + i + " " + QR[i][j]);
-                 }
-              }
-           }
-           Rdiag[k] = -nrm;
-        }
-        /*System.out.println("A");
-        for(int i = 0; i < m; i++) {
-        	for(int j = 0 ; j <n; j++){
-        		System.out.println(QR[i][j]);
-        	}
-        	System.out.println();
-        	}*/
-       
-     }
-
-   
-    public RealMatrix getR () {
-     RealMatrix R = new Array2DRowRealMatrix(qr.getColumnDimension(), qr.getColumnDimension());
-      for (int i = 0; i < col; i++) {
-         for (int j = 0; j < col; j++) {
-            if (i < j) {
-               R.setEntry(i, j, qr.getEntry(i, j));
-            } else if (i == j) {
-               R.setEntry(i, j, dig[i]);
-            } else {
-                R.setEntry(i, j, 0);
-            }
-            System.out.println("R " + i + j + " " + R.getEntry(i, j));
-         }
-      }
-      return R;
-   }
-    
-    
-
-
-    
-    
     public RealMatrix getQ () {
       RealMatrix Q = new Array2DRowRealMatrix(qr.getRowDimension(), qr.getColumnDimension());
       for (int k = col-1; k >= 0; k--) {
@@ -145,19 +69,26 @@ public class qr_fact_househ {
              Q.setEntry(i, k, 0.0);
          }
          Q.setEntry(k, k, 1.0);
+         //System.out.println(Q.getEntry(k, k));
          for (int j = k; j < col; j++) {
             if (qr.getEntry(k, k) != 0) {
                double s = 0.0;
                for (int i = k; i < row; i++) {
                    s = s + (qr.getEntry(i, k) * Q.getEntry(i, j));
+                  // System.out.println(s);
                }
                s = (-s) / qr.getEntry(k, k);
+               //System.out.println();
                for (int i = k; i < row; i++) {
-                   Q.setEntry(i, j, ((s* qr.getEntry(i, k)) + Q.getEntry(i, k)));
+                   Q.setEntry(i, j, ((s* qr.getEntry(i, k)) + Q.getEntry(i, j)));
                }
             }
+           
          }
+         //System.out.println(k);
+         //System.out.println(" " + Q.toString()); 
       }
+      
       return Q;
    }
     
